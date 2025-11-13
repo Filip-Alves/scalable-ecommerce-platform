7964ddf9b5465b0d95c1f87e6ac8efe70d49c02e
Filip-Alves <alvesfilip.work@gmail.com>
2025-11-13 13:03:51 +0100

    feat(payment): update Payment entity, add enums, and initial Flyway migration

--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -2,3 +2,4 @@ CREATE DATABASE product_catalog_db;
 CREATE DATABASE user_service_db;
 CREATE DATABASE shopping_cart_db;
 CREATE DATABASE order_db;
+CREATE DATABASE payment_db;
--- services/payment-service/src/main/java/com/ecommerce/payment_service/model/Payment.java
+++ services/payment-service/src/main/java/com/ecommerce/payment_service/model/Payment.java
@@ -1,61 +1,61 @@
 package com.ecommerce.payment_service.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Entity
 @Table(name = "payments")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Payment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "order_id", nullable = false)
     private Long orderId;
 
+    @Column(name = "user_id", nullable = false)
     private Long userId;
 
+    @Column(nullable = false)
     private BigDecimal amount;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "payment_method", nullable = false)
     private PaymentMethod paymentMethod;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PaymentStatus status;
 
-    @Column(unique = true, updatable = false, nullable = false)
-    private UUID transactionId;
+    @Column(name = "transaction_id", nullable = false, unique = true)
+    private String transactionId;
 
+    @Column(name = "payment_date")
     private LocalDateTime paymentDate;
 
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
     @PrePersist
-    public void prePersist() {
-        if (transactionId == null) {
-            transactionId = UUID.randomUUID();
-        }
-        if (paymentDate == null) {
-            paymentDate = LocalDateTime.now();
-        }
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
     }
 
-    public enum PaymentMethod {
-        CREDIT_CARD,
-        PAYPAL,
-        BANK_TRANSFER
-        // autres ?
-    }
-
-    public enum PaymentStatus {
-        PENDING,
-        SUCCESS,
-        FAILED
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
     }
 }
